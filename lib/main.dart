7f8aa3a980c05d2c6516aeccf59bd14351a8cff4
LucasCVentura <lucascv8525@gmail.com>
2021-03-24 13:58:00 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:alcool_gasolina/GasolinaAlcool.dart';
+import 'package:flutter/material.dart';
+import 'GasolinaAlcool.dart';
+void main() {
+  runApp(MaterialApp(
+    home: GasolinaAlcool(),
+  ));
+}
